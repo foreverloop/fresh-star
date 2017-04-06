5b34fca1725879eab3110e2ad20fc28c3cf6496a
foreverloop <cjones5709@gmail.com>
2017-04-06 19:08:37 +0100

    fix tab issue

--- dice.rb
+++ dice.rb
@@ -1,15 +1,15 @@
 def get_dice_rolls
-	nums = 0
+  nums = 0
 
-	while nums == 0
-		nums = rand(7)
-	end 
-	puts nums
+  while nums == 0
+    nums = rand(7)
+  end 
+  puts nums
 end
 
 dice = 4
 
 while dice > 0
-	get_dice_rolls
-	dice -= 1
+  get_dice_rolls
+  dice -= 1
 end
